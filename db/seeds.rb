a908ecdc64c5eb9364bc50343aecb1af53200235
Julie Kent <jkent2910@gmail.com>
2014-11-09 19:39:37 +0000

    updated seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([
+  { name: 'History'}, {name: 'Biology'}, {name: 'Literature'},
+  { name: 'Mathematics'}, {name: 'Music Theory'}, {name: 'Computer Science'},
+  { name: 'Sociology'}, {name: 'Chemistry'}
+  ])
+
+for i in 0..49
+  title = Faker::Lorem.sentence(rand(2..10)).chomp('.')
+  content = Faker::Lorem.paragraph(word_count=250)
+  
+  category = Category.first(offset: rand(Category.count))
+  a = Article.create(title: title, content: content, categories: [category,])
+end
