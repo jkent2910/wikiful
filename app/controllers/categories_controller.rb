461ee5b20d742912e6ad72d73afc8a53356d729e
Julie Kent <jkent2910@gmail.com>
2014-11-18 16:32:50 +0000

    updated index and show views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,11 @@
 class CategoriesController < ApplicationController
-  def index 
-    @categories = Category.order(:name)
+
+  def index
+    @categories = Category.all.order(:name)
   end
   
   def show
-
+    @category = Category.find(params[:id])
   end
+
 end
